397f7c96ce81d3a7b8afbdf7f64c40a85b57e391
sdlott <sdlott@purdue.edu>
2023-02-08 13:58:20 -0500

    Update bus_factor.py

--- bus_factor.py
+++ bus_factor.py
@@ -2,10 +2,11 @@ import requests
 import os
 
 # GraphQL code from: https://gist.github.com/gbaman/b3137e18c739e0cf98539bf4ec4366ad
-# Bus_Factor link I want to query https://api.github.com/repos/cloudinary/cloudinary_npm/contributors?
+# This code returns the number of forks a repo has to then be used to calculate the bus factor
 
-# This function needs to just return the number of contributors!
+# def getBusFactor
 
+# Not sure how this will work on eceprog, waiting to hear back on piazza
 headers = {"Authorization": "Bearer" + " " + os.environ.get('GITHUB_TOKEN')}
 
 def run_query(query): # A simple function to use requests.post to make the API call. Note the json= section.
@@ -24,8 +25,6 @@ query = """
   }
 }
 """
-
-
 
 result = run_query(query) # Execute the query
 print(result["data"]["repository"]["forkCount"])
