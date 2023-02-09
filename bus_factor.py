54e1048740813372a0c0de4df1f346fec9178262
sdlott <sdlott@purdue.edu>
2023-02-09 14:27:41 -0500

    Working on variables in the query

--- bus_factor.py
+++ bus_factor.py
@@ -4,28 +4,39 @@ import os
 # GraphQL code from: https://gist.github.com/gbaman/b3137e18c739e0cf98539bf4ec4366ad
 # This code returns the number of forks a repo has to then be used to calculate the bus factor
 
-# def getBusFactor
+# getbusfactor takes in the cleaned URL and splits it into user and repo name
+# it then uses a graphQL endpoint to return the number of forks a repo has
+def getbusfactor(clean_url):
+  urlsplit = clean_url.split("/")
+  user = urlsplit[1]
+  repo = urlsplit[2]
+  # Not sure how this will work on eceprog, waiting to hear back on piazza
+  headers = {"Authorization": "Bearer" + " " + os.environ.get('GITHUB_TOKEN')}
 
-# Not sure how this will work on eceprog, waiting to hear back on piazza
-headers = {"Authorization": "Bearer" + " " + os.environ.get('GITHUB_TOKEN')}
+  def run_query(query): # A simple function to use requests.post to make the API call. Note the json= section.
+      request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
+      print(query)
+      if request.status_code == 200:
+          print(request.json())
+          return request.json()
+      else:
+          raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
 
-def run_query(query): # A simple function to use requests.post to make the API call. Note the json= section.
-    request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
-    if request.status_code == 200:
-        return request.json()
-    else:
-        raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
-
-        
-# The GraphQL query (with a few aditional bits included) itself defined as a multi-line string.       
-query = """
-{
-  repository(owner:"cloudinary", name:"cloudinary_npm") {
-    forkCount
+          
+  # The GraphQL query (with a few aditional bits included) itself defined as a multi-line string.     
+  query = """
+  {
+    repository(owner:{user}, name:{repo}) {
+      forkCount
+    }
   }
-}
-"""
+  """.format(user=user, repo=repo)
+  print(query)
 
-result = run_query(query) # Execute the query
-print(result["data"]["repository"]["forkCount"])
+  result = run_query(query) # Execute the query
+  return result["data"]["repository"]["forkCount"]
+
+testURL = "github/cloudinary/cloudinary_npm"
+forkCount = getbusfactor(testURL)
+print(forkCount)
 
